eba6d169367f6fa38d7c7d37cb6947282e70b086
Vlad Minzatu <minzatu.vlad@gmail.com>
2016-03-23 11:35:53 +0100

    Preparing data as arrays

--- util.py
+++ util.py
@@ -7,3 +7,14 @@ def prepare_dataset(data_file):
     data[int_columns] = data[int_columns].astype(float)
     data.replace(-999999.0, 0.0, inplace=True)
     return data
+
+def prepare_train_X_y():
+    train = prepare_dataset('data/train.csv')
+    y = train.TARGET.values
+    X = train.drop(["ID", "TARGET"], axis=1).values
+    return (X,y)
+
+def prepare_test_X():
+    test = prepare_dataset('data/test.csv')
+    X = test.drop(["ID"], axis=1).values
+    return X
